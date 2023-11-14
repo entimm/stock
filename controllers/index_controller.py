e6969980b4d4e221df19802f31d8d2130b4c502a
entimm <entimm@gmail.com>
2023-11-14 12:08:54 +0800

    优化kline

--- controllers/index_controller.py
+++ controllers/index_controller.py
@@ -4,19 +4,25 @@ from enum import Enum, auto
 
 import pandas as pd
 from flask import render_template, Blueprint, request, url_for, redirect
-
 from mootdx.quotes import Quotes
 from mootdx.reader import Reader
-
-from logger import logger
 
 blueprint = Blueprint('index', __name__)
 
 TDX_DIR = '/Volumes/[C] Windows 11/Apps/通达信金融终端(开心果整合版)V2023.03'
 
-FREQUENCY_MINUTE = 8
-FREQUENCY_FIVE_MINUTES = 0
-FREQUENCY_DAILY = 9
+
+class PeriodEnum(Enum):
+    F1 = auto()
+    F5 = auto()
+    D = auto()
+
+
+FREQUENCY_MAP = {
+    PeriodEnum.F1: 8,
+    PeriodEnum.F5: 0,
+    PeriodEnum.D: 9
+}
 
 
 def minutes_since_open():
@@ -26,20 +32,46 @@ def minutes_since_open():
     return math.ceil(diff.total_seconds() / 60)
 
 
-class PeriodEnum(Enum):
-    F1 = auto()
-    F5 = auto()
-    D = auto()
+def fetch_local_data(reader, symbol, period):
+    if period == PeriodEnum.F1:
+        return reader.minute(symbol=symbol)
+    elif period == PeriodEnum.F5:
+        return reader.fzline(symbol=symbol)
+    elif period == PeriodEnum.D:
+        return reader.daily(symbol=symbol)
 
 
-def initialize_tdx():
-    try:
-        reader = Reader.factory(market='std', tdxdir=TDX_DIR)
-        client = Quotes.factory(market='std')
-        return reader, client
-    except Exception as e:
-        logger.error(f"Error during initialization: {e}")
-        return None, None
+def cal_real_offset(period):
+    minutes = minutes_since_open()
+    if period == PeriodEnum.F1:
+        return minutes
+    elif period == PeriodEnum.F5:
+        return math.ceil(minutes)
+    elif period == PeriodEnum.D:
+        return 1
+
+
+@blueprint.route('/')
+def index():
+    symbol = request.args.get('symbol', '', type=str)
+    period = request.args.get('period', '', type=str).upper()
+
+    if not symbol or not period:
+        return redirect(url_for('index.index', symbol='600519', period=PeriodEnum.F5.name))
+
+    reader = Reader.factory(market='std', tdxdir=TDX_DIR)
+    client = Quotes.factory(market='std')
+
+    period_enum = PeriodEnum[period]
+    frequency = FREQUENCY_MAP.get(period_enum)
+
+    local_df = fetch_local_data(reader, symbol, period_enum)
+    real_time_df = client.bars(symbol=symbol, frequency=frequency, offset=cal_real_offset(period_enum))
+
+    df = pd.concat([local_df, pd.DataFrame(real_time_df)], axis=0)
+    kline_list = df.apply(row_to_kline, axis=1).to_list()
+
+    return render_template('index.html', kline_list=kline_list)
 
 
 def row_to_kline(row):
@@ -52,39 +84,3 @@ def row_to_kline(row):
         'amount': row['amount'],
         'volume': row['volume'],
     }
-
-
-def generate_kline_list(df):
-    return [row_to_kline(row) for _, row in df.iterrows()]
-
-
-@blueprint.route('/')
-def index():
-    symbol = request.args.get('symbol', '', type=str)
-    period = request.args.get('period', '', type=str)
-
-    if not symbol or not period:
-        target_url = url_for('index.index', symbol='600519', period=PeriodEnum.F5.name)
-        return redirect(target_url)
-
-    # 初始化 TDX
-    reader, client = initialize_tdx()
-
-    if reader is None or client is None:
-        return render_template('error.html', error_message='Failed to initialize TDX.')
-
-    if period == PeriodEnum.F1.name:
-        df = reader.minute(symbol=symbol)
-        real_time_df = client.bars(symbol=symbol, frequency=FREQUENCY_MINUTE, offset=minutes_since_open())
-    elif period == PeriodEnum.F5.name:
-        df = reader.fzline(symbol=symbol)
-        real_time_df = client.bars(symbol=symbol, frequency=FREQUENCY_FIVE_MINUTES, offset=math.ceil(minutes_since_open() / 5.0))
-    else:
-        df = reader.daily(symbol=symbol)
-        real_time_df = client.bars(symbol=symbol, frequency=FREQUENCY_DAILY, offset=1)
-
-    df = pd.concat([df, pd.DataFrame(real_time_df)], axis=0)
-
-    kline_list = generate_kline_list(df)
-
-    return render_template('index.html', kline_list=kline_list)
