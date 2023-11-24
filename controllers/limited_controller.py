96a67c0536993eb589caf24631a1873a6af0436d
entimm <entimm@gmail.com>
2023-11-24 17:18:00 +0800

    大喜大面

--- controllers/limited_controller.py
+++ controllers/limited_controller.py
@@ -12,8 +12,10 @@ blueprint = Blueprint('limit_stock', __name__)
 @blueprint.route('/limited')
 def limited():
     direction_list = {
-        1: {'name': '涨'},
-        2: {'name': '跌'},
+        1: {'name': '涨停'},
+        2: {'name': '跌停'},
+        3: {'name': '大喜'},
+        4: {'name': '大面'},
     }
     direction = request.args.get('direction', 1, type=int)
 
@@ -25,16 +27,29 @@ def limited():
         df = df[df['ts_code'].str.endswith(('SH', 'SZ'))]
         if direction == 1:
             df = df[df['high'] == df['close']]
-            condition = df['pct_chg'] >= 9.6
-        else:
+            df = df[df['pct_chg'] >= 9.6]
+            df = df.sort_values(by='pct_chg', ascending=False)
+            df['max_result'] = (df['close'] / df['low'] - 1) * 100
+        elif direction == 2:
             df = df[df['low'] == df['close']]
-            condition = df['pct_chg'] <= -9.6
-        df = df[condition]
+            df = df[df['pct_chg'] <= -9.6]
+            df = df.sort_values(by='pct_chg', ascending=True)
+            df['max_result'] = (df['close'] / df['high'] - 1) * 100
+        elif direction == 3:
+            df['max_result'] = (df['close'] / df['low'] - 1) * 100
+            df = df[df['max_result'] >= 9.6]
+            df = df.sort_values(by='max_result', ascending=False)
+        else:
+            df['max_result'] = (df['close'] / df['high'] - 1) * 100
+            df = df[df['max_result'] <= -9.6]
+            df = df.sort_values(by='max_result', ascending=True)
+
         df['pct_chg'] = round(df['pct_chg'], 2)
+        df['max_result'] = round(df['max_result'], 2)
 
         df['ts_code'] = df['ts_code'].str[:6]
         df['ts_name'] = df['ts_code'].map(symbol_name_dict)
-        df['show'] = df['ts_name'].astype(str) + '|' + df['ts_code'].astype(str) + '|' + df['pct_chg'].astype(str)
+        df['show'] = df['ts_name'].astype(str) + '|' + df['ts_code'].astype(str) + '|' + df['pct_chg'].astype(str) + '|' + df['max_result'].astype(str)
 
         result_dict[f'-{date}-'] = df['show'].to_list()
 
