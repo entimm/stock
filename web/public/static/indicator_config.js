a53325428d151456fde17b898df09dccd1e5c574
entimm <entimm@gmail.com>
2023-11-26 18:23:05 +0800

    优化前端，指标配置

---
+++ web/public/static/indicator_config.js
@@ -0,0 +1,15 @@
+const indicator_ma_config = [
+  {period: 5, color: '#930606', size: 1},
+  {period: 10, color: '#ECAB07', size: 1},
+  {period: 20, color: '#EF15DE', size: 1},
+  {period: 60, color: '#16DE45', size: 1.5},
+  {period: 240, color: '#6E28E5', size: 1},
+  {period: 480, color: '#E50B33', size: 1},
+  {period: 960, color: '#16B9DE', size: 1},
+]
+
+const indicator_macd_config = {
+  short: 12,
+  long: 26,
+  signal: 9,
+}
