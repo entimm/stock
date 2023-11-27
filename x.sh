8957442167495a6f9be21ef3825deed3d16ebd89
entimm <entimm@gmail.com>
2023-11-27 17:17:07 +0800

    UPDATE

--- x.sh
+++ x.sh
@@ -1,6 +1,11 @@
+echo 'download-total'
 python cli.py download-total
+echo 'north-funds'
 python cli.py north-funds
 
+echo 'mv-raw'
 python cli.py mv-raw
+echo 'convert-astock'
 python cli.py convert-astock
+echo 'convert-gnbk'
 python cli.py convert-gnbk
