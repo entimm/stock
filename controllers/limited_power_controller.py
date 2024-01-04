b433cf2ca2b8adbba87d07da0b8f158f0de3ea8b
entimm <entimm@gmail.com>
2024-01-05 00:34:20 +0800

    优化涨停动力

--- controllers/limited_power_controller.py
+++ controllers/limited_power_controller.py
@@ -1,6 +1,4 @@
-import json
 import os
-from datetime import datetime
 
 import pandas as pd
 from flask import Blueprint, render_template
@@ -9,7 +7,6 @@ from app_cache import cache
 from common.common import RESOURCES_PATH
 from common.config import config
 from common.quotes import trade_date_list
-from common.xuangubao import row2info
 from controllers import make_cache_key
 
 blueprint = Blueprint('limited_power', __name__)
@@ -17,50 +14,38 @@ blueprint = Blueprint('limited_power', __name__)
 XUANGUBAO_DETAIL_PATH = os.path.join(RESOURCES_PATH, 'xuangubao', 'details')
 
 
-@blueprint.route('/limited_power')
+@blueprint.route('/limited_power_data')
 @cache.cached(timeout=12 * 60 * 60, key_prefix=make_cache_key)
-def limited():
+def limited_power_data():
     result_plate_list = {}
-    result_stock_list = {}
     for ts in trade_date_list.tail(config.get('table_cols', 200))['date'].to_list():
-        plate_dict = {}
-        stock_list = []
         date = ts.strftime('%Y%m%d')
         date2 = ts.strftime('%Y-%m-%d')
         file_path = os.path.join(XUANGUBAO_DETAIL_PATH, f'detail-{date}.csv')
         if not os.path.exists(file_path): continue
         df = pd.read_csv(file_path)
-        for _, row in df.iterrows():
-            related_plates = []
-            if not isinstance(row['surge_reason.related_plates'], float):
-                related_plates = json.loads(row['surge_reason.related_plates'].replace("'", '"'))
-            for item in related_plates:
-                plate_dict.setdefault(item['plate_name'], {'plate_reason': item.get('plate_reason', '空'), 'stock_list': []})['stock_list'].append(row['symbol'])
+        df = df[[
+            'symbol',
+            'stock_chi_name',
+            'limit_up_days',
+            'm_days_n_boards_days',
+            'm_days_n_boards_boards',
+            'first_limit_up',
+            'last_limit_up',
+            'buy_lock_volume_ratio',
+            'non_restricted_capital',
+            'turnover_ratio',
+            'break_limit_up_times',
+            'listed_date',
+            'surge_reason.stock_reason',
+            'surge_reason.related_plates',
+        ]]
+        df.fillna('', inplace=True)
+        result_plate_list[date2] = df.to_dict(orient='records')
 
-            stock_data = row2info(row.to_dict())
-            stock_data['date'] = date2
-            stock_data['plate_names'] = [item['plate_name'] for item in related_plates]
-
-            stock_list.append(stock_data)
-
-        result_plate_list[date2] = [{'plate_name': name, 'plate_reason': item['plate_reason'], 'stock_list': item['stock_list']} for name, item in plate_dict.items()]
-        result_plate_list[date2] = sorted(result_plate_list[date2], key=custom_sort_plate)
-        result_stock_list[date2] = sorted(stock_list, key=lambda x: (-x["m_days_n_boards_boards"], datetime.strptime(x["first_limit_up"], "%H:%M:%S")))
-
-    template_var = {
-        'result_plate_list': dict(reversed(result_plate_list.items())),
-        'result_stock_list': dict(reversed(result_stock_list.items())),
-    }
-
-    return render_template('limited_power.html', **template_var)
+    return result_plate_list
 
 
-def custom_sort_plate(item):
-    plate_priority = {
-        '其他': 1000,
-        '公告': 999,
-        'ST股': 998,
-        '股权转让': 997
-    }
-
-    return plate_priority.get(item['plate_name'], -len(item['stock_list']))
+@blueprint.route('/limited_power')
+def limited_power():
+    return render_template('limited_power.html', **{})
