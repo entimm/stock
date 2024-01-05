ddad358bc8ecb7a2828bf64ac3f9f312800d9247
entimm <entimm@gmail.com>
2024-01-05 11:37:29 +0800

    x2.sh

---
+++ x2.sh
@@ -0,0 +1,11 @@
+echo 'kaipanla-mood'
+python cmd kaipanla-mood
+
+echo 'kaipanla-notice'
+python cmd kaipanla-notice
+
+echo 'kaipanla-limit-up'
+python cmd kaipanla-limit-up
+
+echo 'kaipanla-limit-down'
+python cmd kaipanla-limit-down
