a50d9505b1ee761b6b43cd507bb9c419ee9ba5f3
entimm <entimm@gmail.com>
2024-01-07 22:37:46 +0800

    优化socket

--- web/public/static/socket_utils.js
+++ web/public/static/socket_utils.js
@@ -1,3 +1,23 @@
+let socket = null;
+let selectedRow = null;
+
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowUp' || event.code === 'ArrowDown') {
+    sendSocket(event.code);
+    event.preventDefault();
+  }
+});
+
+function initSocket() {
+  if (socketToken) {
+    socket = io.connect(`http://${document.domain}:${location.port}`);
+    socket.on('message_from_client1', function (message) {
+      if (message.socketToken !== socketToken) return;
+      sendSocket(message.key);
+    });
+  }
+}
+
 function generateRandomString(length) {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -8,6 +28,51 @@ function generateRandomString(length) {
   }
 
   return result;
+}
+
+function sendSocket(direction) {
+  let row = processMove(direction);
+  if (row) {
+    let klineParams = getKlineParams(row);
+    socketEmit(klineParams.symbol, klineParams.date, klineParams.time);
+  }
+}
+
+function processMove(direction) {
+  let row = getAdjacentCell(selectedRow, direction);
+  if (row) {
+    setSelectedRow(row);
+  }
+
+  return row;
+}
+
+function setSelectedRow(row) {
+  if (selectedRow) {
+    selectedRow.classList.remove('select-row');
+  }
+  selectedRow = row;
+  row.classList.add('select-row');
+}
+
+function getRowIndex(row) {
+  return Array.from(row.parentElement.children).indexOf(row);
+}
+
+function getAdjacentCell(row, direction) {
+  if (!row) {
+    row = table.querySelector('tbody tr');
+  }
+  let rowIndex = getRowIndex(row);
+  let numRows = table.querySelector('tbody').childNodes.length;
+  switch (direction) {
+    case "ArrowUp":
+      return rowIndex > 0 ? table.querySelector(`tbody tr:nth-child(${rowIndex})`) : null;
+    case "ArrowDown":
+      return rowIndex < numRows - 1 ? table.querySelector(`tbody tr:nth-child(${rowIndex + 2})`) : null;
+    default:
+      return null;
+  }
 }
 
 function openRelateKlineMode() {
@@ -26,6 +91,7 @@ function openRelateKlineMode() {
 }
 
 function socketEmit(symbol, date = '', time = 0) {
+  if (!socket) return;
   if (!symbol) return;
   socket.emit('message_from_client2', {
     symbol: symbol,
