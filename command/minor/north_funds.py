0321e79e29e6c077d9ba283fb1f2ba3e8a4b8427
entimm <entimm@gmail.com>
2024-01-31 16:16:54 +0800

    UPDATE north_funds

--- command/minor/north_funds.py
+++ command/minor/north_funds.py
@@ -15,11 +15,13 @@ def north_funds():
     current_date = datetime.now().strftime('%Y%m%d')
 
     df = pro.moneyflow_hsgt(start_date='19900101', end_date=current_date, limit=10000)
+    df.set_index('trade_date', inplace=True)
 
     if os.path.exists(NORTH_FUNDS_FILE_PATH):
         local_df = pd.read_csv(NORTH_FUNDS_FILE_PATH)
-        if len(df) <= len(local_df):
-            print(f'忽略，数据大小不比本地的大')
-            return
+        local_df.set_index('trade_date', inplace=True)
+        df = pd.concat([local_df, df]).drop_duplicates()
+
+    df = df.reset_index()
 
     df.to_csv(NORTH_FUNDS_FILE_PATH, index=False)
